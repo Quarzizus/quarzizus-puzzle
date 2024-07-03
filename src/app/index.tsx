8a6bc8a3e24afd58408a92a13ab83bcac2bfc15d
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-02 19:16:16 -0500

    Se dan estilos generales

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,14 @@
+import { Board } from "../components/Board";
+import { Header } from "../components/Header";
 import styles from "./styles.module.css";
 
 function App() {
-  return <main className={styles.App}>Holiwi</main>;
+  return (
+    <main className={styles.App}>
+      <Header />
+      <Board />
+    </main>
+  );
 }
 
 export default App;
