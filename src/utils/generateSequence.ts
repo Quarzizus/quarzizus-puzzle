e7394db22645592e61f347f9eb43c3f373b12e6e
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-09 18:03:52 -0500

    Se crea la función que genera la secuencia

--- src/utils/generateSequence.ts
+++ src/utils/generateSequence.ts
@@ -1,10 +1,25 @@
+interface ItemType {
+  id: number;
+  item: number;
+}
+
 const generateSequence = (length: number) => {
   const numberRandom = Math.floor(Math.random() * (length - 1 + 1)) + 1;
-  const sequence = Array.from({ length: length }).flatMap((_item, i) => {
-    if (i === numberRandom) return [0, i + 1];
-    return i + 1;
+  const sequence: number[] = Array.from({ length: length }).flatMap(
+    (_item, i) => {
+      if (i === numberRandom) return [0, i + 1];
+      return i + 1;
+    }
+  );
+  const sequenceFormated: ItemType[] = sequence.map((item) => {
+    return { id: item, item: item };
   });
-  return sequence;
+  const sequenceRandom = sequenceFormated.sort(() => Math.random() - 0.5);
+  return sequenceRandom;
 };
 
 export { generateSequence };
+// [
+//   { id: 1, item: "shrek" },
+//   { id: 2, item: "fiona" },
+// ]
