81cb4ae4289aca4cc0f4972348bc3a91e3f1e03e
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-12 12:18:06 -0500

    Se renombran variables para una mejor lectura

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,51 +1,36 @@
 import { generateSequence } from "../../utils/generateSequence";
 import { Box, BoxVoid } from "../Box";
+import { BoxType } from "../Box/types";
 import { Container } from "./styles";
 import { useState } from "react";
-
-interface ItemType {
-  id: number;
-  item: number;
-}
+import { ReSort } from "./types";
 
 const Board = () => {
-  const [list, setList] = useState<ItemType[]>(() => generateSequence(24));
+  const [list, setList] = useState<BoxType[]>(() => generateSequence(24));
 
-  const reSort = (index: number, indexVoid: number, item: number) => {
-    const newList: ItemType[] = list.map((e, i) => {
+  const reSort = ({ index, indexVoid, value }: ReSort) => {
+    const newList: BoxType[] = list.map((e, i) => {
       if (i === index) {
-        return { id: 0, item: 0 };
+        return { id: 0, value: 0 };
       }
       if (i === indexVoid) {
-        return { id: item, item: item };
+        return { id: value, value: value };
       }
       return e;
     });
 
     setList(newList);
-  };
-
-  const handle = (item: number) => {
-    const index = list.findIndex((value) => value.item === item);
-    const indexVoid = list.findIndex((value) => value.item === 0);
-
-    if (
-      index - 1 === indexVoid ||
-      index + 1 === indexVoid ||
-      index + 5 === indexVoid ||
-      index - 5 === indexVoid
-    ) {
-      reSort(index, indexVoid, item);
-      return;
-    }
+    return newList;
   };
 
   return (
     <Container>
-      {list.map(({ id, item }) => {
-        if (item !== 0)
-          return <Box key={id} item={item} id={id} handle={handle} />;
-        if (item === 0) return <BoxVoid key={id} />;
+      {list.map(({ id, value }) => {
+        if (value !== 0)
+          return (
+            <Box key={id} value={value} id={id} list={list} handle={reSort} />
+          );
+        if (value === 0) return <BoxVoid key={id} />;
       })}
     </Container>
   );
