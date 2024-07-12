054c154135c32a066c67f2f2d31abdb11e5d86c1
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-12 12:18:52 -0500

    Se crea la función para validar si es un movimiento correcto

---
+++ src/utils/toValidate.ts
@@ -0,0 +1,44 @@
+import { BoxType } from "../components/Box/types";
+
+interface Props {
+  list: BoxType[];
+  boxValue: number;
+}
+
+enum Positions {
+  top = "top",
+  right = "right",
+  bottom = "bottom",
+  left = "left",
+}
+
+interface ToValidate {
+  isValidate: boolean;
+  position: Positions;
+  index: number;
+  indexVoid: number;
+}
+
+const toValidate = ({ list, boxValue }: Props): ToValidate => {
+  const index = list.findIndex((box) => box.value === boxValue);
+  const indexVoid = list.findIndex((box) => box.value === 0);
+  const results: ToValidate = {
+    isValidate: false,
+    position: Positions.top,
+    index,
+    indexVoid,
+  };
+  if (index - 1 === indexVoid)
+    (results.isValidate = true), (results.position = Positions.left);
+  if (index + 1 === indexVoid)
+    (results.isValidate = true), (results.position = Positions.right);
+  if (index - 5 === indexVoid)
+    (results.isValidate = true), (results.position = Positions.top);
+  if (index + 5 === indexVoid)
+    (results.isValidate = true), (results.position = Positions.bottom);
+
+  return results;
+};
+
+export { toValidate };
+export type { Positions };
