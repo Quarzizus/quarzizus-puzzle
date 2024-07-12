df7000fa1c9a20490cd6f38476da9bb4534eb935
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-12 12:17:12 -0500

    Se instala firebase para el hosting

---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSUF6fn9-IjT4h6sBdEe5tfM7sO-meP7Y",
+  authDomain: "qz-puzzle.firebaseapp.com",
+  projectId: "qz-puzzle",
+  storageBucket: "qz-puzzle.appspot.com",
+  messagingSenderId: "51592546945",
+  appId: "1:51592546945:web:540d501c25881e8b4bdb80",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
