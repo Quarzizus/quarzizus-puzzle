24114790442e04ee509788d49b5ac4ff7537587b
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-12 12:20:23 -0500

    Se emplean las funcipnes en Box

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,17 +1,88 @@
+import { useRef } from "react";
 import { BoxComponent, BoxVoidComponent } from "./styles";
+import { BoxType } from "./types";
+import { Positions, toValidate } from "../../utils/toValidate";
+import { createTransition } from "../../utils/createTransition";
+import { ReSort } from "../Board/types";
 
-interface Props {
-  id: number;
-  item: number;
-  handle: (item: number) => void;
+interface Props extends BoxType {
+  handle: ({ index, indexVoid, value }: ReSort) => BoxType[];
+  list: BoxType[];
 }
 
-const Box = ({ item, id, handle }: Props) => {
-  return <BoxComponent onClick={() => handle(id)}>{item}</BoxComponent>;
+const Box = ({ value, handle, list }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const getWidth = (element: HTMLDivElement | null) => {
+    const bounding = element?.getBoundingClientRect();
+    return bounding?.width || 0;
+  };
+  const moveBoxVoid = (position: Positions) => {
+    const refVoid = document.getElementsByClassName(
+      "BoxVoid"
+    )[0] as HTMLDivElement;
+    const width = getWidth(refVoid);
+    const contraryPositions = {
+      top: "bottom",
+      bottom: "top",
+      left: "right",
+      right: "left",
+    };
+    const styles = createTransition({
+      width,
+      position: contraryPositions[position],
+    });
+    refVoid.style.transform = styles;
+    refVoid.style.transition = "1s linear";
+  };
+  const moveBox = (position: Positions) => {
+    const width = getWidth(ref?.current);
+    const styles = createTransition({ width, position });
+    if (ref.current?.style) {
+      ref.current.style.transform = styles;
+      ref.current.style.transition = "1s linear";
+    }
+  };
+  const removeStyles = () => {
+    const refVoid = document.getElementsByClassName(
+      "BoxVoid"
+    )[0] as HTMLDivElement;
+    if (ref.current?.style) {
+      ref.current.style.transform = "";
+      refVoid.style.transform = "";
+      ref.current.style.transition = "";
+      refVoid.style.transition = "";
+    }
+  };
+  const handleBox = async () => {
+    const { isValidate, position, index, indexVoid } = toValidate({
+      list,
+      boxValue: value,
+    });
+    if (!isValidate) return;
+
+    await new Promise((resolve) => {
+      moveBox(position);
+      moveBoxVoid(position);
+
+      resolve(
+        setTimeout(() => {
+          removeStyles();
+          handle({ index, indexVoid, value });
+        }, 1000)
+      );
+    });
+  };
+
+  return (
+    <BoxComponent ref={ref} onClick={() => handleBox()}>
+      {value}
+    </BoxComponent>
+  );
 };
 
 const BoxVoid = () => {
-  return <BoxVoidComponent></BoxVoidComponent>;
+  return <BoxVoidComponent className="BoxVoid"></BoxVoidComponent>;
 };
 
 export { Box, BoxVoid };
