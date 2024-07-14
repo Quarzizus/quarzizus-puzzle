874cf343725dfc51dfca31abf94c7359624cdb68
Miguel Vásquez <angelzizu06@gmail.com>
2024-07-13 21:13:04 -0500

    Se genera un container para el board y el timer

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import { Board } from "../components/Board";
 import { Header } from "../components/Header";
+import { Timer } from "../components/Timer";
 import styles from "./styles.module.css";
 
 function App() {
   return (
     <main className={styles.App}>
       <Header />
-      <Board />
+      <div className={styles.container}>
+        <Board />
+        <Timer />
+      </div>
     </main>
   );
 }
